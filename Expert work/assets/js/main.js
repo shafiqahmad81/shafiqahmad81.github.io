f461aeae73bf8fb56b5b21f456cca50af2b41e5c
shafiqahmad81 <77017494+shafiqahmad81@users.noreply.github.com>
2021-06-16 12:20:40 +0600

    Its new work

---
+++ Expert work/assets/js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+	$('.logo-carousel').owlCarousel({
+	    items:5,
+	    margin:30,
+	    dots:false,
+	    loop:true,
+	    nav:false,
+
+	    
+	})
+	$('.casestudies-carousel').owlCarousel({
+	    items:3,
+	    loop:true,
+	    margin:30,
+	    dots:true,
+	    nav:false,
+	    
+	})
+	
+	$('.testimonial-carousel').owlCarousel({
+	    items:1,
+	    loop:true,
+	    margin:false,
+	    dots:true,
+	    nav:false,
+	    
+	})
+	
+});
+
+
+
+	
+
+	
